f33c62abe7a7b32451e617017ae68f545116d60c
TheDarkBug <adrianoliviero23@gmail.com>
2022-04-29 17:44:46 +0200

    Completed first usable version

--- app/src/main/java/com/tdb/numstring/MainActivity.kt
+++ app/src/main/java/com/tdb/numstring/MainActivity.kt
@@ -1,27 +1,35 @@
 package com.tdb.numstring
 
+import android.content.res.AssetManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.PopupMenu
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.System.loadLibrary
 
 class MainActivity : AppCompatActivity() {
+    var numstringLang: String = "italian"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadLibrary("num_string")
-        findViewById<EditText>(R.id.userInput).setText(greeting("Worqweld"))
     }
 
-    external fun greeting(pattern: String): String
+    fun readLangFile(name: String): String {
+        var file_name: String = "langs/english.txt"
+        if (name.equals("italian"))
+            file_name = "langs/italian.txt"
+        else if (name.equals("english"))
+            file_name = "langs/english.txt"
+        return application.assets.open(file_name).bufferedReader().use{it.readText()}
+    }
 
-    fun showPopup(view: View) {
-        var popup = PopupMenu(this, view)
-        var inflater = popup.menuInflater
-        inflater.inflate(androidx.core.R.menu.example_menu, popup.menu)
-        popup.show()
+    private external fun convert(input: String, lang: String): String
+
+    fun convertOnClick(view: View) {
+        findViewById<TextView>(R.id.output).text = convert(findViewById<EditText>(R.id.userInput).text.toString(), readLangFile(numstringLang))
     }
 }
