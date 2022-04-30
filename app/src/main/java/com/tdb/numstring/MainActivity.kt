a0570935125707fefbd8b16121bd82d35005fd9f
TheDarkBug <adrianoliviero23@gmail.com>
2022-04-30 11:40:38 +0200

    Added click-to-copy on output textview (not working)

--- app/src/main/java/com/tdb/numstring/MainActivity.kt
+++ app/src/main/java/com/tdb/numstring/MainActivity.kt
@@ -1,34 +1,52 @@
 package com.tdb.numstring
 
-import android.content.res.AssetManager
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
-import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.System.loadLibrary
 
+
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.convert_button).setOnClickListener {
+            findViewById<TextView>(R.id.output).text = convert(findViewById<EditText>(R.id.userInput).text.toString(), readLangFile(getString(R.string.convert_lang)))
+        }
+        findViewById<Button>(R.id.clear_button).setOnClickListener {
+            findViewById<TextView>(R.id.output).text = ""
+            findViewById<EditText>(R.id.userInput).setText("")
+        }
+        findViewById<TextView>(R.id.output).setOnClickListener {
+            copyToClipboard()
+        }
         loadLibrary("num_string")
     }
 
-    fun readLangFile(name: String): String {
-        var file_name: String = "langs/english.txt"
-        if (name.equals("italian"))
+    private fun readLangFile(name: String): String {
+        var file_name = "langs/english.txt"
+        if (name == "italian")
             file_name = "langs/italian.txt"
-        else if (name.equals("english"))
+        else if (name == "english")
             file_name = "langs/english.txt"
         return application.assets.open(file_name).bufferedReader().use{it.readText()}
     }
 
+    private fun copyToClipboard() {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip: ClipData = ClipData.newPlainText("output", findViewById<TextView>(R.id.output).toString())
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, R.string.text_copied, Toast.LENGTH_LONG).show()
+    }
+
     private external fun convert(input: String, lang: String): String
 
-    fun convertOnClick(view: View) {
-        findViewById<TextView>(R.id.output).text = convert(findViewById<EditText>(R.id.userInput).text.toString(), readLangFile(getString(R.string.convert_lang)))
-    }
 }
